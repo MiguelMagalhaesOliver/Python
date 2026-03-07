0637f03fe7dbd275d23430af14cf482c0111b3b9
Miguel Magalhães <miguelmagoliveira@gmail.com>
2026-03-07 18:26:06 -0300

    Add methods to consult and list stock products

--- SISTEMA.py
+++ SISTEMA.py
@@ -38,4 +38,34 @@ class Home:
         else:
             print(f"{produto} não encontrado em estoque.")
 
-    def 
+    def consultar_estoque(self, produto):
+        cursor = self.conn.fetchone()
+        cursor.execute(
+            "SELECT quantidade FROM estoque WHERE produto=?", (produto,))
+        resultado = cursor.fetchone()
+        if resultado:
+            return resultado[0]
+        else:
+            return 0
+        
+    def listar_produtos(self):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT produto FROM estoque")
+        produtos = cursor.fetchall()
+        return [produto[0] for produto in produtos]
+    
+sistema = Home("Estoque.db")
+
+sistema.adicionar_produto("Mangá: Jujutsu Kaisen volume 1", 50)
+sistema.adicionar_produto("Mangá: YuYu Hakusho volume 1", 49)
+sistema.adicionar_produto("Mangá: JoJo's Bizarre Adventures - Parte 6: Stone Ocean volume 1", 25)
+
+estoque_manga = sistema.consultar_estoque("Mangás")
+print(f"Quantidade de Mangás em estoque: {estoque_manga}")
+
+
+sistema.remover_produto("Mangá: JoJo's Bizarre Adventures - Parte 6: Stone Ocean volume 1", 10)
+print("Estoque de Mangás alterados com sucesso!")
+
+produtos_em_estoque = sistema.listar_produtos()
+print(f"Produtos em estoque: {produtos_em_estoque}")
